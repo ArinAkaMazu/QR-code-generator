5765566a2dd240c4a07f92973ad599b221f63a36
ArinAkaMazu <arinkaushik60@gmail.com>
2024-02-14 14:06:55 +0530

    minor fixes

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
 import qrcode
 from PIL import Image
 import re
-
-
 url_input = input("Enter a valid link: ")
-
-
 pattern = re.search("((https?://)?(www\.)?)?[a-zA-Z0-9\-._]+\.[a-zA-Z]{2,3}", url_input)
-
 if pattern:
     qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=4)
     qr.add_data(url_input)
